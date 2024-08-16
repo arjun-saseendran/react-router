9b0eb550c0ad49a16c9ad98679596d9122d401f8
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-16 14:59:12 +0530

    add tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='bg-green-500'>testing tailwind</div>
+  )
+}
+
+export default App
