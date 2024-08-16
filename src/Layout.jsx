2b893537feb2fc0fac9f1bd362b970910f113abc
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-16 16:51:51 +0530

    add layout

---
+++ src/Layout.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+import Header from './components/Header/Header';
+import Footer from './components/footer/Footer';
+
+const Layout = () => {
+  return (
+    <>
+        
+    </>
+  )
+}
+
+export default Layout
