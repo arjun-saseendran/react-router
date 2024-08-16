c03dc1abaab3b239464199cfe71473e6bf7f794e
arjun-saseendran <arjunsaseendran@icloud.com>
2024-08-16 17:26:16 +0530

    add about

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,17 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import Home from './components/home/Home.jsx'
 import Layout from './Layout.jsx'
 import './index.css'
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+import About from './components/about/About.jsx'
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path='/' element={<Layout/>}>
-
+    <Route path='' element={<Home/>}/>
+    <Route path='about' element={<About/>}/>
     </Route>
   )
 );
